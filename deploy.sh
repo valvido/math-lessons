6c20e033b9d0ea17caec830fba0cfc560251edaa
valvido <vidovicv33@gmail.com>
2021-10-11 23:28:10 +0200

    Uppate config

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:valvido/valvido.github.io.git master
+# git push -f git@github.com:valvido/valvido.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:valvido/math-lessons.git main:gh-pages
 
 cd -
