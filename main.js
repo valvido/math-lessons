4644e0acfb5d6a52e2569496a02a432f5c591ce9
valvido <vidovicv33@gmail.com>
2021-10-15 16:30:34 +0200

    Uppdate styles

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import "./style.css";
+const cssModule = await import("./style.css", {
+  assert: { type: "css" },
+});
+document.adoptedStyleSheets = [cssModule.default];
 
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navbar__menu");
